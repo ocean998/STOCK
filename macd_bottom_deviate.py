5ec25f764b3497f1f2181de8314bd2b3e05698e7
ocean998 <40862700+ocean998@users.noreply.github.com>
2019-03-14 16:41:22 +0800

    底背离

---
+++ macd_bottom_deviate.py
@@ -0,0 +1,11 @@
+import baostock as bs
+import pandas as pd
+import numpy as np
+import macd_base as mb
+
+
+if __name__ == '__main__':
+    macd_60 = mb.MACD_INDEX('60')
+    # macd_60.set_time('2018-06-01','2018-12-11')
+
+    macd_60.save_bottom('D:\\0_stock_macd\\_周K线金叉.xls', False)
