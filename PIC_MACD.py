d71f61b9b0dc736e38ada97b4aada564526eab5b
ocean998 <40862700+ocean998@users.noreply.github.com>
2019-03-15 22:13:02 +0800

    return raise

--- PIC_MACD.py
+++ PIC_MACD.py
@@ -109,18 +109,101 @@ class MACD_INDEX:
 
         return xx
 
+    def analyze_dead(self, macd, isprt=False):
+        ''' macd最后一次交叉是死叉，才有返回值，返回包括 死叉日期，死叉后红柱数量，绿柱高度，快线高度
+            save_golden方法计算完macd后调用，后续应继续调用判断 是否即将金叉的方法 analyze_bing_golden
+        '''
+        rst = pd.DataFrame(columns=[macd.columns.values])
+        cnt_lv = 0
+        cnt = macd.shape[0]
+        if cnt < 3:
+            return rst
+        # isprt是否打印输入的macd数组，用于调试
+        # if isprt:
+        #     print(macd)
+        # 如果当前macd红柱表示已经金叉，不判断直接返回
+        if macd.iloc[-1]['macd'] > 0:
+            return rst
+        for i in range(1, cnt - 1):
+            # macd>0 为红柱，计数并向前找小于0的时间为金叉点
+            if macd.iloc[-i]['macd'] < 0:
+                cnt_lv += 1  # 绿柱计数
+                continue
+            else:
+                rst = macd.iloc[-i + 1:].reset_index(drop=True)
+                return rst
+
+    def analyze_bing_golden(self, macd, isprt=False):
+        ''' 用于判断是否将要金叉macd已经死叉还未金叉，如果快线斜率在3个周期内发生金叉则返回
+            macd死叉后的macd指标，pandas.DataFrame类型 save_golden方法计算完macd发生死叉后调用，
+            零轴下死叉适用 frame.iloc[frame['test2'].idxmin()]['test3']
+        '''
+
+        rst = []
+        dead_macd = self.analyze_dead(macd, isprt)
+        dead_len = dead_macd.shape[0]
+        if dead_len == 0:
+            # 不符合死叉条件，不判断即将金叉
+            return rst
+
+        # 死叉macd为负值 所以取最小值
+        idx_min = dead_macd['macd'].idxmin()
+        # 绿线最低值发生在当前
+        if dead_len == idx_min + 1:
+            return rst
+        # 取最低点后的数据
+        macd_jc = dead_macd.iloc[idx_min:].reset_index(drop=True)
+        dead_len = macd_jc.shape[0]
+        if isprt:
+            print('\nMACD最低点后的数据是：\n')
+            print(macd_jc)
+        if dead_len > 3:
+            x = []
+            # 最低点后如果有3根以上的柱子再判断趋势，缩短y，加长n
+            for idx in range(0, dead_len - 1):
+                if macd_jc.iloc[idx]['macd'] - \
+                        macd_jc.iloc[(idx + 1)]['macd'] >= 0:
+                    x.append('n')
+                else:
+                    if isprt:
+                        print(idx,
+                              ':',
+                              macd_jc.iloc[idx]['macd'],
+                              '-',
+                              macd_jc.iloc[(idx + 1)]['macd'])
+                    x.append('y')
+            if x[-1] == 'y':
+                rst.append('即将金叉')
+            if isprt:
+                print('\nMACD最低点后的 趋势\n', x)
+        else:
+            # 不足4跟
+            if dead_macd.iloc[-1]['macd'] <= dead_macd.iloc[-2]['macd']:
+                rst.append('即将金叉')
+
+        if len(rst) == 1:
+            rst.append(self.code.split('.')[1])
+            rst.append(dead_macd.iloc[0]['time'])
+            rst.append(dead_macd.iloc[-1]['macd'])
+            rst.append(dead_macd.iloc[-2]['macd'])
+            rst.append(self.code)
+            return rst
+        else:
+            return []
 
 if __name__ == "__main__":
     # 60 即将金叉
     macd_60 = MACD_INDEX('60')
-    data = macd_60.get_index('sz.000725')
+    data = macd_60.get_index('sh.600088')
     macd = macd_60.get_MACD(data)
     macd.set_index('time')
-    print(macd)
+
     print('macd 图形')
+    df3 = macd_60.analyze_bing_golden( macd )
+    print( df3 )
 
     # macd.plot(x='time',y='macd',y='dif',color='black', y='dea', color='yello')
     # macd.plot(x='time', y='macd',color='blue', y='dif', color='black')
-    macd.plot(x='time',y = ['dif','macd','dea'], color=['blue','green','yellow'])
-    plt.show()
+    # macd.plot(x='time',y = ['dif','macd','dea'], color=['blue','green','yellow'])
+    # plt.show()
 # pip install matplotlib
