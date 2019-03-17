f9c090a7fd631edbf85e0873b2b95367e97f8754
ocean998 <40862700+ocean998@users.noreply.github.com>
2019-03-18 00:22:58 +0800

    return raise

--- macd_t_60jc.py
+++ macd_t_60jc.py
@@ -4,5 +4,8 @@ import macd_base as mb
 
 if __name__ == "__main__":
     # 60 即将金叉
+    macd_w = mb.MACD_INDEX( 'w' )
+    macd_w.save_golden( 'D:\\0_stock_macd\\_月K线金叉.xls' )
+
     macd_60 = mb.MACD_INDEX('60')
-    macd_60.save_bing_golden('all', False)
+    macd_60.save_bing_golden('D:\\0_stock_macd\\_周K线金叉.xls'  False)
